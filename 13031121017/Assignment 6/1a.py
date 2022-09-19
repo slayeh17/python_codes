8ad001372c4469e3617716a5782c6967c546b0c5
Debak Roy <104914725+slayeh17@users.noreply.github.com>
2022-09-19 18:55:57 +0530

    Delete 1a.py

--- 13031121017/Assignment 6/1a.py
+++
@@ -1,9 +0,0 @@
-dic={}
-n=int(input("Enter the number of values: "))
-for i in range(n):
-	key=input("Enter the key: ")
-	value=input("Enter the value: ")
-	dic[key]=value
-print("Original Dictionary: ",dic)
-dicnew=dict(sorted(dic.items()))
-print("Sorted: ",dicnew)
